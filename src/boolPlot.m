36826b35a6047f1c2b405b16204c23a7980e7276
thomas <frost.tj@protonmail.com>
2020-05-04 13:49:49 +0100

    iniital content

---
+++ src/boolPlot.m
@@ -0,0 +1,105 @@
+function hPlot = boolPlot(data, time)
+
+openNewFigure = false;
+figTag = 'boolPlotFig';
+figName = 'Boolean Plot';
+tsData = isa(data,'timeseries');
+numData = isnumeric(data);
+boolHeight = 1;
+boolSpace = 0.5;
+trueLable = '';
+falseLabel = '';
+
+assert(tsData || numData,'data must be numeric or a timeseries')
+
+if openNewFigure
+    hPlot = figure('Name',figName, 'tag',figTag);
+    hAxe = axes(hPlot);
+else
+    hPlot = findobj(0,'tag',figTag);
+    if isempty(hPlot)
+        hPlot = figure('Name',figName', 'tag',figTag);
+        hAxe = axes(hPlot);
+    else
+        clf(hPlot);
+        hAxe = axes(hPlot);
+    end
+end
+
+cM = get(0,'DefaultAxesColorOrder');
+cN = size(cM,1);
+
+if numData
+    if isrow(data)
+        data = transpose(data);
+    end
+    if nargin < 2
+        tsData = timeseries(data,0:length(data)-1);
+    else
+        assert(isrow(time),'time nust be a row vector')
+        if (find(size(data) == numel(time),1) ~= 1)
+            data = transpose(data);
+        end
+        tsData = timeseries(data,time);
+    end
+elseif tsData
+    tsData = data;
+end
+
+[tsPlot, nPlot] = compressTimeseries(tsData);
+yOffset = 0;
+yTickLabel = cell(1,nPlot*3);
+minT = inf;
+maxT = 0;
+
+for n = 1 : nPlot
+    idx = (n-1)*3+1;
+    yTickLabel(idx:idx+2) = {falseLabel,['Bit' num2str(n-1)],trueLable};
+    y = tsPlot(n).Data;
+    x = tsPlot(n).Time;
+    minT = min(minT,min(x));
+    maxT = max(maxT,max(x));
+    rbg = cM(mod(n-1,cN)+1,:);
+    Y = [];
+    if isvector(x) && ismatrix(y) && size(y,1)==length(x)
+        x = reshape(x,length(x),1);   
+    else
+        error('x and y dimentions should be consistent.')
+    end
+    for k = 1 :size(y,2)
+        x = [x';x'];
+        temp = [y(:,k)';y(:,k)'];
+        temp = temp(:);
+        Y(:,k) = temp; %#ok<AGROW>
+        xPlot = [x([2:end end]), fliplr(x([2:end end]))];
+        yPlot = boolHeight * [Y; zeros(length(xPlot)/2,size(Y,2))];
+        patch(hAxe,'XData',xPlot,'YData',yPlot+yOffset,'FaceColor',rbg);
+        yOffset = yOffset + boolHeight + boolSpace;
+    end
+end
+
+yTickPos = [0,cumsum(repmat([boolHeight/2, boolHeight/2,boolSpace],1,nPlot))];
+
+set(hAxe,'YTick',yTickPos(1:end-1))
+set(hAxe,'YTickLabel',yTickLabel);
+set(hAxe,'TickLength',[0 0]);
+set(hAxe,'XLim',[minT maxT]);
+set(hAxe,'YLim',[0 yOffset]);
+
+end
+
+function [tsOutArray, nElem] = compressTimeseries(tsIn)
+    nTime = numel(tsIn.Time);
+    find(size(tsIn.Data) == nTime)
+    nElem = numel(tsIn.Data)/nTime;
+    allData = boolean(tsIn.Data);
+    indexAllData = @(n) (n-1)*nTime+1:(n-1)*nTime+nTime;
+    tsOutArray(nElem) = timeseries;
+    for n = 1 : nElem
+        elemData = allData(indexAllData(n));
+        deltaIdx = [true; (diff(elemData(1:end-1))~=0)'; true];
+        deltaData = elemData(deltaIdx);
+        deltaTime = tsIn.Time(deltaIdx);
+        tsOutArray(n) = timeseries(deltaData',deltaTime);
+    end
+end
